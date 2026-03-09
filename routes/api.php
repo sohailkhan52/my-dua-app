91c85b9d2dcf9d8002ad61d6e88cbcd94ee3021f
Sohail Khan <sohailkhan52@gmail.com>
2026-03-09 09:47:39 +0500

    google login re,oved and managed the auth to login for only one user

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Api\AuthController;
 
 Route::post("/login",[AuthController::class,'login']);
 Route::post("/logout",[AuthController::class,'logout']);
